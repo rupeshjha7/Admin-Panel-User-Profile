0c6f9f084e2115e16943dbfe2f2ccabb5e7c8059
rupeshjha7 <jharupesh223@gmail.com>
2024-03-21 13:11:32 +0530

    user admin profile

--- assets/js/stepform.js
+++ assets/js/stepform.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         return this.optional(element) || /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/i.test(value);
     }, "please provide valid email address");
 
-    $(".next").click(function formsubmit () {
+    $(".next").click(function () {
         var form = $("#myform");
         form.validate({
             errorElement: 'span',
